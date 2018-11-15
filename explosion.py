1bdfb1f404d8520490064a5e7e135454f5fd78a5
Steve Rich <steven.john.rich@gmail.com>
2018-11-15 15:45:40 +0000

    Added colour by fellow contributor

--- explosion.py
+++ explosion.py
@@ -2,8 +2,6 @@ import pgzrun
 import random
 import math
 
-
-
 # the size of the screen
 WIDTH = 800
 HEIGHT = 600
@@ -12,22 +10,32 @@ HEIGHT = 600
 DRAG = 0.8 
 
 # the colour of each particle in R, G, B values
-PARTICLE_COLOR = 255, 230, 128
+# Now with Original, White, Red, Green and Yellow (you can add more if you want)
+COLOURS = []
+COLOURS.append((255, 230, 128))
+COLOURS.append((255, 255, 255))
+COLOURS.append((255, 0, 0))
+COLOURS.append((0, 255, 0))
+COLOURS.append((0, 0, 255))
+COLOURS.append((255, 255, 0))
 
 # the time in seconds for which a particle is displayed
 MAX_AGE = 3
 
-# a list to hold the details of the explosion particles on the screen
+# an list to hold the details of the explosion particles on the screen
 particles = []
-
 
 # This function creates a new explosion at the specified screen co-ordinates
 
-def explode(x, y, speed=300):
+def explode(x, y, speed=300, colour = None):
 
     # these are new particles, so set their age to zero
     age = 0     
     
+    # grab a random colour if one isn't specified
+    if colour is None:
+        colour = random.choice(COLOURS)
+
     # generate 100 particles per explosion
     for _ in range(100):
     
@@ -39,9 +47,8 @@ def explode(x, y, speed=300):
         vx = speed * radius * math.sin(angle)
         vy = speed * radius * math.cos(angle)
         
-        # add the particle's position, velocity and age to the list
-        particles.append((x, y, vx, vy, age))
-
+        # add the particle's position, colour, velocity and age to the list
+        particles.append((x, y, colour, vx, vy, age))
 
 # This function redraws the screen by plotting each particle in the list
 
@@ -51,11 +58,10 @@ def draw():
     screen.clear()
     
     # loop through all the particles in the list
-    for x, y, *_ in particles:
+    for x, y, colour, *_ in particles:
         
         # for each particle in the list, plot its position on the screen
-        screen.surface.set_at((int(x), int(y)), PARTICLE_COLOR)
-
+        screen.surface.set_at((int(x), int(y)), colour)
 
 # This function updates the list of particles
 
@@ -65,7 +71,7 @@ def update(dt):
     new_particles = []
     
     # loop through the existing particle list
-    for (x, y, vx, vy, age) in particles:
+    for (x, y, colour, vx, vy, age) in particles:
     
         # if a particle was created more than a certain time ago, it can be removed
         if age + dt > MAX_AGE:
@@ -83,27 +89,27 @@ def update(dt):
         # update the particle's age
         age += dt
         
-        # add the particle's new position, velocity and age to the new list
-        new_particles.append((x, y, vx, vy, age))
+        # add the particle's new positionm colour, velocity and age to the new list
+        new_particles.append((x, y, colour, vx, vy, age))
         
     # replace the current list with the new one
     particles[:] = new_particles
 
-
 # This function creates an explosion at a random location on the screen
 
 def explode_random():
-
     # select a random position on the screen
     x = random.randrange(WIDTH)
     y = random.randrange(HEIGHT)
     
-    # call the explosion function for that position
-    explode(x, y)
+    # call the explosion function for that position (with a little randomness to the speed of the explosion)
+    explode(x, y, random.randint(200, 500))
+
+# Randomise the random thingy
+random.seed()
 
 # call the random explosion function every 1.5 seconds
-# 
 clock.schedule_interval(explode_random, 1.5)
 
-
 pgzrun.go()
+
